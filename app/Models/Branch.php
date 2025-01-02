6f202cfa35fe7cc7d4d875338478d50ca0b3533b
Iqbal Maulana A <iqbalmaulanaasyari17@gmail.com>
2025-01-02 14:47:59 +0700

    membuat model setiap migration dan mengisi nama table, fillable, dan relasi relasinya

---
+++ app/Models/Branch.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Branch extends Model
+{
+    protected $table = 'branches';
+    protected $fillable = ['name','location'];
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = 'products';
+    protected $fillable = ['name','price','stock','branch_id'];
+    public function branches()
+    {
+        return $this->belongsTo(Branch::class);
+    }
+    public function transactions()
+    {
+        return $this->belongsToMany(Transaction::class, 'transaction_detail', 'product_id', 'transaction_id');
+    }
+}
